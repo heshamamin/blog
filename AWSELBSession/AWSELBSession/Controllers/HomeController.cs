9c42e92e89e5f01a601fb4def468761a9e0ba991
Hesham A. Amin <hspceng@hotmail.com>
2016-02-27 15:20:02 +0200

    Initial AWSELBSession

---
+++ AWSELBSession/AWSELBSession/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AWSELBSession.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			HttpClient httpClient = new HttpClient();
+			string hostName = httpClient.GetStringAsync("http://169.254.169.254/latest/meta-data/public-hostname").Result;
+			string ipv4 = httpClient.GetStringAsync("http://169.254.169.254/latest/meta-data/public-ipv4").Result;
+
+			ControllerContext.HttpContext.Session["HostName"] = hostName;
+			ControllerContext.HttpContext.Session["IPV4"] = ipv4;
+
+			ViewBag.HostName = hostName;
+			ViewBag.IP = ipv4;
+
+			return View();
+		}
+
+		public ActionResult About()
+		{
+			ViewBag.Message = "Your application description page.";
+
+			return View();
+		}
+
+		public ActionResult Contact()
+		{
+			ViewBag.Message = "Your contact page.";
+
+			return View();
+		}
+	}
+}
---
+++ AWSELBSession/AWSELBSession/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AWSELBSession.MvcApplication" Language="C#" %>
