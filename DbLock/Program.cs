d0448505be6d8a831315a98d918335376dcff48d
Hesham A. Amin <hspceng@hotmail.com>
2017-04-26 20:04:58 +1000

    Adding DbLock

---
+++ DbLock/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data.Common;
+using System.Data.SqlClient;
+
+namespace DbLock
+{
+    class Program
+	{
+		static void Main(string[] args)
+		{
+            using (DbConnection connection = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=AdventureWorks2014;Integrated Security=True;Application Name=""My DbLock App"""))
+            {
+                var command = connection.CreateCommand();
+                command.CommandText = @"
+                        Begin transaction
+                        update Person.EmailAddress
+                        set EmailAddress = '-'+EmailAddress";
+                command.CommandType = System.Data.CommandType.Text;
+
+                connection.Open();
+                using (var transaction = connection.BeginTransaction())
+                {
+                    command.Transaction = transaction;
+                    command.ExecuteNonQuery();
+                    Console.ReadLine();
+                }
+            }
+               
+        }
+	}
+}
